3c4e67865659e6bbc4c3623dd8026d3c564f5d6b
Matthew Danque <mattdanque@gmail.com>
2024-04-13 22:57:05 -0500

    Add files via upload
    KNN ASPECT

---
+++ CS412_KNN.py
@@ -0,0 +1,6 @@
+import numpy as np
+
+#Testimg Data To Use
+poker_test = np.loadtxt("poker-hand-testing.data", dtype='str')
+#Training Data To Use
+poker_train = np.loadtxt("poker-hand-training-true.data", dtype='str')
